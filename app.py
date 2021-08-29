af70c996ca97c7321691ee232660d24f620fd33c
johan_mitma12 <avira365@hotmail.com>
2021-08-29 04:53:11 -0500

    mejoras agregadas

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
+im = Image.open("resources/redv2.jpg")
+st.set_page_config(
+        page_title="Deteccion de COVID19",
+        page_icon=im,
+       
+)
+
+
 @st.cache
 def load_model():
     
@@ -36,11 +44,12 @@ def show_image_gradcam(model,image_array,col):
 def show_results(predictions):
     class_result=np.argmax(predictions)
     if class_result==0:
-        st.markdown("La radiografia corresponde a un paciente con **COVID-19**. Por su salud, acuda a un centro de vacunacion")
+       
+        st.error("La radiografia corresponde a un paciente con **COVID-19**. Por su salud, acuda a un centro de vacunacion mas Cercano.")
     elif class_result==1:
-        st.markdown("La radiografia corresponde a un paciente con una **ENFERMEDAD PULMONAR** distinta al COVID-19. Acuda a un medico para su tratamiento")
+        st.warning("La radiografia corresponde a un paciente con una **ENFERMEDAD PULMONAR** distinta al COVID-19. Acuda a un medico para su tratamiento. Posiblemente se trate de neumonia viral o Lung Opacity.")
     elif class_result==2:
-        st.markdown("La radiografia  de rayos-X corresponde a un **paciente NORMAL**")
+        st.success("La radiografia  de rayos-X corresponde a un **PACIENTE NORMAL**")
     st.subheader("Precision de diagnostico")
     col1,col2,col3=st.columns(3)
     col1.metric("COVID-19","{0:0.2f}%".format(predictions[0]*100))
@@ -49,6 +58,7 @@ def show_results(predictions):
 
 def main():
     st.title("Modelo para la deteccion de COVID-19 en base a radiografias")
+    st.subheader("Desarrollador: Johan Mitma - UNMSM -2021")
     file_image=st.file_uploader("Seleccione un archivo de imagen",type=["png","jpg","jpeg"])
 
     model=load_model()
@@ -62,7 +72,13 @@ def main():
         show_image_gradcam(model,array_img,col2)
         predictions=get_prediction(model,image_array)
         show_results(predictions)
-
+    else:
+        st.success("Por favor, suba una radiografia de rayos-X para su descarte de COVID-19")
+        
+        st.image(Image.open("./resources/portada_xray.jpg"))
+    st.markdown("*Investigacion dedicada a mi madre **Isabel Huaccha Fernandez** *")
+    st.markdown("**Usar responsablemente:** Modelo con **92%** de precision")
+    st.markdown("**Acerca del autor**: [Linkedin](https://www.linkedin.com/in/johan-valerio-mitma12/) | [IG](https://www.instagram.com/johan_mitma12/)")
 
 if __name__=="__main__":
     main()
